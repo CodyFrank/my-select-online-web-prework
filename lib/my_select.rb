c1d34fb6d4ba7e461dfeea2fbb75a2b7ee701dec
Cody Frank <cody.frank30@gmail.com>
2019-03-24 23:36:56 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+ if block_given?
+   index = 0
+   while index < array.size
+     yeild(array[index])
+     index += 1
+   end
+ end
 end
