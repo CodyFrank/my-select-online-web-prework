7c6924ba0515123b4927a51e5390412b4dc0451f
Cody Frank <cody.frank30@gmail.com>
2019-03-25 00:34:08 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(array)
  if block_given?
    index = 0
    while index < array.size
+     new_array = []
      yield(array[index])
-        new_array = []
         new_Array << array[index] if yield(array[index])
     index += 1
    end
